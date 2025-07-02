c5efc2712b1740abcd26a57fd8e4c6ab5453f0fd
sinianluoye <sinianluoye@outlook.com>
2025-07-03 01:19:25 +0800

    finish LeastUpperBoundProperty for real

--- Rudin/Chapter1/Real.lean
+++ Rudin/Chapter1/Real.lean
@@ -32,6 +32,10 @@ theorem ext {a b : Cut} (hab : a.carrier = b.carrier) : a = b := by
   cases hab
   simp
 
+
+theorem mem_iff_mem_carrier {x:ℚ} {a:Cut} : x ∈ a ↔ x ∈ a.carrier := by
+  simp [instMembershipℚCut]
+
 theorem ne_iff_carrier_ne {a b:Cut} : a ≠ b ↔ a.carrier ≠ b.carrier := by
   constructor
   intro h h1
@@ -68,6 +72,20 @@ instance : LE Cut where
   le a b := a.carrier ≤ b.carrier
 
 
+
+theorem lt_then_ex_not_in_carrier {a b:Cut} (h: a < b): ∃ x ∈ b, x ∉ a := by
+  simp [instLT, ← Set.ssub_iff_lt, Set.ssub_def] at h
+  rcases h with ⟨h1, x, hx1, hx2⟩
+  use x
+  simp [instMembershipℚCut]
+  constructor
+  exact hx1
+  exact hx2
+
+
+
+
+
 theorem le_iff_not_gt {a b:Cut} : a ≤ b ↔ ¬ a > b := by
   simp [Cut.instLT, Cut.instLE, ← Set.ssub_iff_lt, ← Set.sub_iff_le]
   constructor
@@ -89,6 +107,7 @@ theorem le_iff_not_gt {a b:Cut} : a ≤ b ↔ ¬ a > b := by
   have hxyb := in_lt_not_in hyb hxb
   exact a.lt_then_in hxa hxyb
   use x
+
 
 instance : Ordered Cut where
 
@@ -140,6 +159,14 @@ instance : Ordered Cut where
       rw [h]
     exact h1.mpr (Or.inr this)
 
+theorem le_def {a b:Cut} : a ≤ b ↔ (∀ x, x ∈ a.carrier → x ∈ b.carrier) := by
+  simp [Real.instMembershipℚCut, instLE, ← Set.sub_iff_le, Set.sub_def]
+
+
+theorem lt_def {a b:Cut} : a < b ↔ (∀ x, x ∈ a.carrier → x ∈ b.carrier) ∧ ∃ x ∈ b.carrier, x ∉ a.carrier:= by
+  simp [lt_iff_le_not_le, le_def]
+
+
 
 end Cut
 
@@ -179,21 +206,77 @@ instance : LeastUpperBoundProperty ℝ where
         have h3:= (h x)
         rcases h3 with ⟨ α, ha⟩
         have h4:= hA2 α ha.left
-        simp [Real.Cut.instOrdered, Real.Cut.instLE] at h4
-        simp [← Set.sub_iff_le] at h4
-        simp [Set.sub_def] at h4
+        simp [Real.Cut.le_def] at h4
         exact h4 x ha.right
       exact h1 h2
 
     have carr_lt_then_in {p q:ℚ} (hp: p ∈ carr) (hq: q < p): q ∈ carr := by
-      sorry
+      simp [carr] at hp
+      rcases hp with ⟨ α , ha1, ha2⟩
+      simp [carr]
+      use α
+      constructor
+      exact ha1
+      exact α.lt_then_in ha2 hq
 
     have carr_ex_gt {p:ℚ} (hp: p ∈ carr): ∃ r ∈ carr, p < r := by
-      sorry
+      simp [carr] at hp
+      rcases hp with ⟨ α , ha1, ha2⟩
+      have h1 := α.ex_gt ha2
+      rcases h1 with ⟨ r, hr1, hr2⟩
+      use r
+      constructor
+      simp [carr]
+      use α
+      constructor
+      exact ha1
+      exact hr1
+      exact hr2
 
     let γ:ℝ := ⟨carr, carr_ne_empty, carr_ne_univ,  carr_lt_then_in, carr_ex_gt⟩
     use γ
-    sorry
+    by_cases h: ∃ δ, δ < γ ∧ Sup A δ
+    rcases h with ⟨ δ, h1, h2 ⟩
+    have h3:= Real.Cut.lt_then_ex_not_in_carrier h1
+    rcases h3 with ⟨ s, hs1, hs2⟩
+    simp [Real.instMembershipℚCut] at hs1
+    rw [Set.mem_setOf] at hs1
+    simp [Sup, UpperBound] at h2
+    rcases h2 with ⟨ h3, h4⟩
+    rcases hs1 with ⟨ α , ha1, ha2⟩
+    have h5 := h3 α ha1
+    have h6 : s ∈ δ := by
+      simp [Real.Cut.le_def] at h5
+      rw [Real.Cut.mem_iff_mem_carrier] at ha2
+      have := h5 s ha2
+      rw [← Real.Cut.mem_iff_mem_carrier] at this
+      exact this
+    exfalso
+    exact hs2 h6
+    simp [Sup] at *
+    constructor
+    simp [UpperBound]
+    intro α hα
+    simp [Real.Cut.le_def]
+    intro s hs
+    rw [Set.mem_setOf]
+    use α
+    constructor
+    exact hα
+    exact hs
+    intro δ h1 h2
+    simp only [Real.Cut.lt_def] at h1
+    rcases h1 with ⟨ h3, h4⟩
+    rcases h4 with ⟨ s, hs1, hs2⟩
+    rw [Set.mem_setOf] at hs1
+    rcases hs1 with ⟨ α , hα1, hα2⟩
+    simp [UpperBound] at h2
+    have h5:= h2 α hα1
+    simp [Real.Cut.le_def] at h5
+    have h6 := h5 s hα2
+    exact hs2 h6
+
+
 
 
 
